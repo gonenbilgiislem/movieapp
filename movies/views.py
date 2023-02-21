b9ef0e2e8ce17232ffdb444f54f935f144dddffd
gonenbilgiislem <bilgiislem@balikesirgonen.bel.tr>
2023-02-22 00:59:09 +0300

    ...

--- movies/views.py
+++ movies/views.py
@@ -1,18 +1,47 @@
 from django.shortcuts import render
 
-kategoriler = ["macera", "romantik", "dram"]
+kategori_listele = ["macera", "romantik", "dram", "komedi"]
 # Create your views here.
-
+film_liste = [
+    {
+        "film_adi": "film 1",
+        "aciklama": "film 1 açıklama",
+        "resim": "1.jpg",
+        "anasayfa": True
+    },
+    {
+        "film_adi": "film 2",
+        "aciklama": "film 2 açıklama",
+        "resim": "2.jpg",
+"anasayfa": True
+    },
+    {
+        "film_adi": "film 3",
+        "aciklama": "film 3 açıklama",
+        "resim": "3.jpg",
+        "anasayfa": False
+    },
+    {
+        "film_adi": "film 4",
+        "aciklama": "film 4 açıklama",
+        "resim": "4.jpg",
+        "anasayfa": False
+    }
+]
 def home(request):
     data = {
-    "kategori": kategoriler
+    "kategoriler": kategori_listele,
+    "filmler": film_liste,
+    "title": "Ana Sayfa"
     }
     return render(
-        request, 'index.html',data
-    )
+        request, 'index.html',data)
 
 def movies(request):
-    return render(request, 'movies.html',
-                  {'title': 'Filmler',
-                   'message': 'Filmler Sayfasına Hoş Geldiniz.'}
-                  )
+
+    data = {
+    "kategoriler": kategori_listele,
+    "filmler": film_liste,
+    "title"  : "Filmler"
+    }
+    return render(request,"movies.html" ,data)
