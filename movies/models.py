cd510b20e713d94a8ca01cea6badd3aaf619572e
baris.ozen <bilgiislem@balikesirgonen.bel.tr>
2023-02-24 17:18:09 +0300

    .

--- movies/models.py
+++ movies/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
 class Movie(models.Model):
-    title = models.CharField(max_length=100)
+    film_adi = models.CharField(max_length=100)
     aciklama = models.TextField()
-    resim = models.ImageField(upload_to='images/')
-    category = models.CharField(max_length=100)
-    date = models.DateTimeField(auto_now_add=True)
-    def __str__(self):
-        return self.title
+    resim = models.TextField(max_length=100)
+    anasayfa =  models.BooleanField(default=False)
+    kayit_tarihi = models.DateTimeField(auto_now_add=True)
--- movies/views.py
+++ movies/views.py
@@ -1,42 +1,11 @@
 from django.shortcuts import render
+from .models import Category, Movie
 
-kategori_listele = ["macera", "romantik", "dram", "komedi"]
-# Create your views here.
-film_liste = [
-    {
-        "id": "1",
-        "film_adi": "film 1",
-        "aciklama": "film 1 açıklama",
-        "resim": "1.jpeg",
-        "anasayfa": True
-    },
-    {
-        "id": "2",
-        "film_adi": "film 2",
-        "aciklama": "film 2 açıklama",
-        "resim": "2.jpeg",
-"anasayfa": True
-    },
-    {
-        "id": "3",
-        "film_adi": "film 3",
-        "aciklama": "film 3 açıklama",
-        "resim": "3.jpeg",
-        "anasayfa": False
-    },
-    {
-        "id": "4",
-        "film_adi": "film 4",
-        "aciklama": "film 4 açıklama",
-        "resim": "4.jpeg",
-        "anasayfa": False
-    }
-]
 def home(request):
     data = {
-    "kategoriler": kategori_listele,
-    "filmler": film_liste,
-    "title": "Ana Sayfa"
+    "kategoriler": Category.objects.all(),
+    "filmler": Movie.objects.filter(anasayfa=True),
+    "title"  : "Ana sayfa"
     }
     return render(
         request, 'index.html',data)
@@ -44,14 +13,14 @@ def home(request):
 def movies(request):
 
     data = {
-    "kategoriler": kategori_listele,
-    "filmler": film_liste,
+    "kategoriler": Category.objects.all(),
+    "filmler": Movie.objects.all(),
     "title"  : "Filmler"
     }
     return render(request,"movies.html" ,data)
 
 def moviedetail(request,id):
     data = {
-    "id": id
+    "movie": Movie.objects.get(id=id)
     }
     return render(request,"details.html" ,data)
